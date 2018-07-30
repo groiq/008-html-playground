905923bf922b1dd32066930a15fc9c0d1978d6dc
groiq <35578027+groiq@users.noreply.github.com>
2018-07-30 14:14:41 +0200

    loops exercise

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-document.write("<p>Ovid<br />\"Ich habe ein Werk geschaffen...\"</p>");
+var i = 0;
+var j = 0;
+while(j <= 1000) {
+    i++;
+    j = i * i;
+    document.write(i + ": " + j + "<br />");
+}
