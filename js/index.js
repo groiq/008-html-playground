1b5bdd044282a5326e62b8f53b95446b84e69d13
groiq <35578027+groiq@users.noreply.github.com>
2018-07-31 12:58:47 +0200

    generate arithemetic table with loops

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-var i = 5;
+function prod(j,k) {
+    var result;
+    if (j == 0) { result = k; }
+    else if (k == 0) { result = j; }
+    else { result = j*k; }
+    return result;
+}
+
+var i = 33;
 
 document.write("<table border='1'>");
 
 for (var j = 0; j < i; j++) {
     document.write("<tr>");
-        document.write("<td>&nbsp;</td>");
+        for (var k = 0; k < i; k++) {
+            document.write("<td width ='5'>" + prod(j,k) + "</td>");
+        }
     document.write("</tr>");
 }
 
