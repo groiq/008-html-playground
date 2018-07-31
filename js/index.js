002163c714e079f3017f37a5f92c2abee9378ed3
groiq <35578027+groiq@users.noreply.github.com>
2018-07-31 13:35:45 +0200

    multiply variant

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@ function prod(j,k) {
     var result;
     if (j == 0) { result = k; }
     else if (k == 0) { result = j; }
-    else { result = j^k; }
-    result = "rgb(" + result + "," + result + "," + result + ")"
+    else { result = j*k; }
+    result = "rgb(0,0," + result + ")"
     return result;
 }
 
-var i = 255;
+var i = 17;
 
-document.write("<table>");
+document.write("<table style='color:#ffff00'>");
 
 for (var j = 0; j < i; j++) {
     document.write("<tr>");
         for (var k = 0; k < i; k++) {
-            // document.write("<td style='background-color:" + prod(j,k) + "'>" + prod(j,k) + "</td>");
-            document.write("<td style='background-color:" + prod(j,k) + "'></td>");
+            document.write("<td style='background-color:" + prod(j,k) + "'>" + prod(j,k) + "</td>");
+            // document.write("<td style='padding:100em, background-color:" + prod(j,k) + "'>&nbsp;</td>");
         }
     document.write("</tr>");
 }
