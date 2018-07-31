2611b1e7692465602e86d1a254e4c698569aacf8
groiq <35578027+groiq@users.noreply.github.com>
2018-07-31 13:41:49 +0200

    blue to cyan

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@ function prod(j,k) {
     var result;
     if (j == 0) { result = k; }
     else if (k == 0) { result = j; }
-    else { result = j*k; }
-    result = "rgb(0,0," + result + ")"
+    else { result = j^k; }
+    result = "rgb(0," + result + ",255)"
     return result;
 }
 
-var i = 17;
+var i = 255;
 
-document.write("<table style='color:#ffff00'>");
+document.write("<table>");
 
 for (var j = 0; j < i; j++) {
     document.write("<tr>");
         for (var k = 0; k < i; k++) {
-            document.write("<td style='background-color:" + prod(j,k) + "'>" + prod(j,k) + "</td>");
-            // document.write("<td style='padding:100em, background-color:" + prod(j,k) + "'>&nbsp;</td>");
+            // document.write("<td style='background-color:" + prod(j,k) + "'>" + prod(j,k) + "</td>");
+            document.write("<td style='background-color:" + prod(j,k) + "'></td>");
         }
     document.write("</tr>");
 }
