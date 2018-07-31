54956e5c1b5e8e9b65e5a1bd1612131c12df0f8f
groiq <35578027+groiq@users.noreply.github.com>
2018-07-31 10:52:43 +0200

    set up an example about recursive functions

---
+++ js/matryoshka.js
@@ -0,0 +1,10 @@
+//a little story about matryoshkas.
+
+document.write("<p>A wooden doll is standing on the table. The sage old man says, 'This is a matryoshka. They sometimes contain little pearls of wisdom.'</p>");
+
+function openMatryoshka(count,total) {
+    document.write("I open matryoshka number " + count + ".");
+    if (count < total) {  }
+}
+
+openMatryoshka(0,4);
