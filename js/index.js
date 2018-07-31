10daffabb9322581ffcd9a185fae2275fa0015eb
groiq <35578027+groiq@users.noreply.github.com>
2018-07-31 10:25:49 +0200

    exercise in functions

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-var i = 0;
-var j = 0;
-while(j <= 1000) {
-    i++;
-    j = i * i;
-    document.write(i + ": " + j + "<br />");
+function doSomething(i) {
+    var result = "";
+    var a = 2;
+    var b = 3;
+    var c = 0;
+    switch (i) {
+        case 0: c = a + b++; break;
+        case 1: c = a + (++b); break;
+        case 2: c = a + b--; break;
+        case 3: c = a + (--b); break;
+        default: c = "invalid"; break;
+    }
+    result = a + " und " + b + " und " + c + "<br />";
+    //result = result + "<br />";
+    return result;
+}
+
+for (var i = 0; i<5; i++) {
+    //i++;
+    document.write(doSomething(i));
 }
