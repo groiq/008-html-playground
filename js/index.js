9f80d405062421899fd5c061eb58e47fa14ec698
groiq <35578027+groiq@users.noreply.github.com>
2018-08-01 09:23:23 +0200

    generate function calls with varying numbers of arguments

--- js/index.js
+++ js/index.js
@@ -1,23 +1,9 @@
-function prod(j,k) {
-    var result;
-    if (j == 0) { result = k; }
-    else if (k == 0) { result = j; }
-    else { result = j^k; }
-    result = "rgb(0," + result + ",255)"
-    return result;
+
+
+for (var i=0; i<11; i++) {
+    document.write("add(");
+    for (var j=0; j<(i+1); j++) {
+        document.write(j + ", ");
+    }
+    document.write(");<br />");
 }
-
-var i = 255;
-
-document.write("<table>");
-
-for (var j = 0; j < i; j++) {
-    document.write("<tr>");
-        for (var k = 0; k < i; k++) {
-            // document.write("<td style='background-color:" + prod(j,k) + "'>" + prod(j,k) + "</td>");
-            document.write("<td style='background-color:" + prod(j,k) + "'></td>");
-        }
-    document.write("</tr>");
-}
-
-document.write("</table>");
--- js/magicscript.js
+++ js/magicscript.js
@@ -1,1 +1,1 @@
-
+defAFunc();
