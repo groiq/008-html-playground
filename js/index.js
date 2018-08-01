e3c8e1095fac90e9a9edf76a17965491d597606d
groiq <35578027+groiq@users.noreply.github.com>
2018-08-01 11:29:50 +0200

    conditional function definitions (failed)

--- js/index.js
+++ js/index.js
@@ -1,32 +1,22 @@
-
-
-for (var i=0; i<11; i++) {
-    document.write("document.write(add(");
-    for (var j=0; j<(i+1); j++) {
-        document.write(j);
-        if (j<(i)) {
-            document.write(", ");
+function sel(shape) {
+    if (shape == "dreieck") {
+        function calc(length) {
+            return (length * length);
         }
     }
-    document.write(") + '&lt;br /&gt;');<br />");
+    else if (shape == "quadrat") {
+        function calc(length) {
+            return (length * length / 2);
+        }
+    }
+    
 }
 
-function add() {
-    var result = 0;
-    for (var i=0;i<arguments.length;i++) {
-        result += i;
-    }
+function a(shape, length) {
+    sel(shape);
+    var result = calc(length);
     return result;
 }
 
-document.write(add(0) + '<br />');
-document.write(add(0, 1) + '<br />');
-document.write(add(0, 1, 2) + '<br />');
-document.write(add(0, 1, 2, 3) + '<br />');
-document.write(add(0, 1, 2, 3, 4) + '<br />');
-document.write(add(0, 1, 2, 3, 4, 5) + '<br />');
-document.write(add(0, 1, 2, 3, 4, 5, 6) + '<br />');
-document.write(add(0, 1, 2, 3, 4, 5, 6, 7) + '<br />');
-document.write(add(0, 1, 2, 3, 4, 5, 6, 7, 8) + '<br />');
-document.write(add(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) + '<br />');
-document.write(add(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10) + '<br />');
+document.write(a("quadrat",2));
+document.write(a("dreieck",2));
