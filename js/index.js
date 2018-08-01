5689e5cc0fad04c940c56cc7de39851d483e37f2
groiq <35578027+groiq@users.noreply.github.com>
2018-08-01 11:50:33 +0200

    alert message with an anonymous function

--- js/index.js
+++ js/index.js
@@ -1,17 +1,1 @@
-yellMode = "normal";
-if (yellMode == "normal") {
-    function yell(content) { document.write(content); }
-}
-else if (yellMode == "obtrusive") {
-    function yell(content) { alert(content); }
-}
-yell("Welt!");
-
-yellMode = "obtrusive";
-if (yellMode == "normal") {
-    function yell(content) { document.write(content); }
-}
-else if (yellMode == "obtrusive") {
-    function yell(content) { alert(content); }
-}
-yell("Hallo!");
+alert(function() {return "hello!";}());
