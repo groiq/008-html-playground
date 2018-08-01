7cfde4909ad8e022aaf66f6573d225a7d685470a
groiq <35578027+groiq@users.noreply.github.com>
2018-08-01 09:32:22 +0200

    tricks with variable parameter lists

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ for (var i=0; i<11; i++) {
     }
     document.write(") + '&lt;br /&gt;');<br />");
 }
+
+function add() {
+    var result = 0;
+    for (var i=0;i<arguments.length;i++) {
+        result += i;
+    }
+    return result;
+}
+
+document.write(add(0) + '<br />');
+document.write(add(0, 1) + '<br />');
+document.write(add(0, 1, 2) + '<br />');
+document.write(add(0, 1, 2, 3) + '<br />');
+document.write(add(0, 1, 2, 3, 4) + '<br />');
+document.write(add(0, 1, 2, 3, 4, 5) + '<br />');
+document.write(add(0, 1, 2, 3, 4, 5, 6) + '<br />');
+document.write(add(0, 1, 2, 3, 4, 5, 6, 7) + '<br />');
+document.write(add(0, 1, 2, 3, 4, 5, 6, 7, 8) + '<br />');
+document.write(add(0, 1, 2, 3, 4, 5, 6, 7, 8, 9) + '<br />');
+document.write(add(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10) + '<br />');
