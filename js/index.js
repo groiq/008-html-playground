b8d01a74a66b34ec249149faec56c3fc26f7f19d
groiq <35578027+groiq@users.noreply.github.com>
2018-08-01 12:17:00 +0200

    testing datetime manipulation

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-var yellie = function() { return "Hello!"; }
-alert(yellie());
+var now = new Date();
+var morning = now.setHours(8,0,0,0);
+var afternoon = now.setHours(16,0,0,0);
+var br = "<br />";
+
+document.write(now + br + morning + br + afternoon);
+
+/* Output:
+Wed Aug 01 2018 16:00:00 GMT+0200
+1533103200000
+1533132000000 
+*/
