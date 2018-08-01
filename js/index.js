6a981efb7b50516aa8d84c12dd74e7e3993b43b6
groiq <35578027+groiq@users.noreply.github.com>
2018-08-01 09:27:30 +0200

    debug

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 
 
 for (var i=0; i<11; i++) {
-    document.write("add(");
+    document.write("document.write(add(");
     for (var j=0; j<(i+1); j++) {
-        document.write(j + ", ");
+        document.write(j);
+        if (j<(i)) {
+            document.write(", ");
+        }
     }
-    document.write(");<br />");
+    document.write("));<br />");
 }
