9a9fabedd11692a95cf542682ca33f597b321084
groiq <35578027+groiq@users.noreply.github.com>
2018-08-02 13:48:11 +0200

    Public and private variables

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-function absatzAusgabe() {
-    for (var i = 0; i < arguments.length; i++) {
-        document.write(arguments[i] + "<br />");
-    }
+var pubName = "Bruce Wayne";
+var secretName;
+
+function tell(pubName, secretName) {
+    document.write("<p>");
+    document.write("Public name: ");
+    document.write(pubName);
+    document.write("<br />");
+    document.write("Secret identity:");
+    document.write(secretName);
+    document.write("</p>");
 }
 
-absatzAusgabe("hallo",12345);
-absatzAusgabe(3,7.4,"nein", true);
+function secret() {
+    var secretName = "Batman";
+    tell(pubName,secretName);
+}
+
+secret();
+tell(pubName,secretName);
+
+/* Output:
+Public name: Bruce Wayne
+Secret identity:Batman
+
+Public name: Bruce Wayne
+Secret identity:undefined
+
+*/
