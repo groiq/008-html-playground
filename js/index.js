9145f2f7ca099e888c14305df01948de41b36eb6
groiq <35578027+groiq@users.noreply.github.com>
2018-08-02 10:55:36 +0200

    exercise: variable parameter lists

--- js/index.js
+++ js/index.js
@@ -1,14 +1,8 @@
-var now = new Date();
-var morning = now;
-var afternoon = now;
-morning.setHours(8,0,0,0);
-afternoon.setHours(16.0,0,0);
-var br = "<br />";
+function absatzAusgabe() {
+    for (var i = 0; i < arguments.length; i++) {
+        document.write(arguments[i] + "<br />");
+    }
+}
 
-document.write(now + br + morning + br + afternoon);
-
-/* Output:
-Wed Aug 01 2018 16:00:00 GMT+0200
-Wed Aug 01 2018 16:00:00 GMT+0200
-Wed Aug 01 2018 16:00:00 GMT+0200 
-*/
+absatzAusgabe("hallo",12345);
+absatzAusgabe(3,7.4,"nein", true);
