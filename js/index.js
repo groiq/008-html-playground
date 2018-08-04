55d31573514e64352990af54a35a6cb9e7b04de0
groiq <35578027+groiq@users.noreply.github.com>
2018-08-04 14:54:15 +0200

    creating a single object

--- js/index.js
+++ js/index.js
@@ -1,22 +1,11 @@
-function character(race,profession,weapon) {
-    this.race = race;
-    this.profession = profession;
-    this.weapon = weapon;
-}
+var gavin = {
+    name: "Gavin of Anywhere",
+    race: "human",
+    profession: "warrior",
+    weapon: {
+        type: "sword",
+        range: "melee",
+        hand: "main hand"
+    }
+};
 
-function weapon(range,hand) {
-    this.range = range;
-    this.hand = hand;
-}
-
-
-var Gavin = new character("human","warrior",new weapon("melee","main hand"));
-
-function print(text) {
-    document.write(text + "<br />");
-}
-
-print(Gavin);
-print(Gavin.profession);
-print(Gavin.weapon);
-print(Gavin.weapon.hand);
