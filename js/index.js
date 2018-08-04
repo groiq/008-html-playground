6c79808b11b02e8bfbd000119bcf2b454ffd022f
groiq <35578027+groiq@users.noreply.github.com>
2018-08-04 15:14:12 +0200

    object methods

--- js/index.js
+++ js/index.js
@@ -6,13 +6,29 @@ var gavin = {
         type: "sword",
         range: "melee",
         hand: "main hand"
-    }
+    },
+    greet: function() {
+    result = "Hello, I'm " + this.name;
+    document.write(result);
+}
 };
 
 for (var field in gavin) {
     document.write(field);
     document.write(": ");
-    //document.write(gavin.field);
     document.write(gavin[field]);
+    document.write(", not ");
+    document.write(gavin.field);
     document.write("<br />");
 }
+
+gavin.greet();
+
+/*output:
+name: Gavin of Anywhere, not undefined
+race: human, not undefined
+profession: warrior, not undefined
+weapon: [object Object], not undefined
+greet: function() { result = "Hello, I'm " + this.name; document.write(result); }, not undefined
+Hello, I'm Gavin of Anywhere 
+*/
