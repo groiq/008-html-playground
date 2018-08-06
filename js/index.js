8d1562791e37be9565bfa1e746fe1da6e326c822
groiq <35578027+groiq@users.noreply.github.com>
2018-08-06 17:09:42 +0200

    add a method at runtime

--- js/index.js
+++ js/index.js
@@ -1,56 +1,16 @@
-
-function say(text) {
-    document.write(text);
-    document.write("<br />");
-}
-
-var continents = ["europe","america"];
-
-function city(continent) {
-    this.continent = continent;
-}
-
-var paris = new city(continents[0]);
-var athens = new city(continents[0]);
-var la = new city(continents[1]);
-
-var truck = {
-    name: "truck",
-    cargo: "solid",
-    location: paris,
-    //range: this.location.continent,
-    //range: this.location.continent,
-    range: function() {
-        return this.location.continent;
-    },
-    kmCost: 10,
-    cost: function(dist) {
-        return (this.kmCost * dist);
+var mesmer = {
+    profession: "mesmer",
+    main: "sword",
+    wield: function() {
+        return "The " + this.profession + " wields his " + this.main + "!<br />";
     }
 };
 
-var tankTruck = Object.create(truck);
-tankTruck.name = "Tank truck";
-tankTruck.cargo = "liquid";
-tankTruck.location = la;
+document.write(mesmer.wield());
 
-var plane = Object.create(truck);
-plane.name = "Cargo Plane";
-plane.airportCost = 100;
-plane.cost = function(dist) {
-    return ((this.kmCost * dist) + this.airportCost);
-};
-plane.range = function() {
-    return continents;
+mesmer.offhand = "pistol";
+mesmer.offWield = function() {
+    return "Now the " + this.profession + " wields his " + this.offhand + "!";
 };
 
-var transporters = [truck, tankTruck, plane];
-
-for (var transporterIndex in transporters) {
-    var transporter = transporters[transporterIndex];
-    say(transporter.name);
-    say(transporter.cargo);
-    say(transporter.range());
-    say(transporter.cost(100));
-    say("---");
-}
+document.write(mesmer.offWield());
