53a278e3523f1194bd54eb12d3f0d6036efda1b8
groiq <35578027+groiq@users.noreply.github.com>
2018-08-08 09:10:31 +0200

    testing inheritance in prototype-based OO

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-var chlegg = {
-    name: "chlegg",
-    race: "asura",
-    profession: "mesmer",
-    title: "psychotect"
-};
-
-for (var field in chlegg) {
-    document.write(field + ": " + chlegg[field] +  "<br />");
+function say(text) {
+    document.write(text);
+    document.write("<br />");
 }
+
+var obj1 = {
+    size: 5,
+}
+
+say(obj1.size);
+
+var thing2 = Object.create(obj1);
+
+say(thing2.size);
+
+obj1.size = 4;
+
+say(obj1.size);
+say(thing2.size);
+
+/*Output:
+5
+5
+4
+4
+
+*/
