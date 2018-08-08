1aeb3de60eaf00c5169d7b626e802afc0248a153
groiq <35578027+groiq@users.noreply.github.com>
2018-08-08 12:13:52 +0200

    put say() function into external script

--- js/index.js
+++ js/index.js
@@ -1,27 +1,2 @@
-function say(text) {
-    document.write(text);
-    document.write("<br />");
-}
-
-var obj1 = {
-    size: 5,
-}
-
-say(obj1.size);
-
-var thing2 = Object.create(obj1);
-
-say(thing2.size);
-
-obj1.size = 4;
-
-say(obj1.size);
-say(thing2.size);
-
-/*Output:
-5
-5
-4
-4
-
-*/
+say("hello?");
+say("does that work?");
