6f03c445210146e0c1ca505335645f326e68f160
groiq <35578027+groiq@users.noreply.github.com>
2018-08-10 15:02:17 +0200

    exercise in tables

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 
 var stats = ["staerke","geschick","wille","magie","klugheit","konstitution"];
 var stats = {
-    staerke: 10,
-    geschick: 10,
-    wille: 10,
-    magie: 10,
-    klugheit: 10,
-    konstitution: 10
+    staerke: 0,
+    geschick: 1,
+    wille: 1,
+    magie: 2,
+    klugheit: 2,
+    konstitution: 0
 };
 
 function val(x) {
@@ -16,26 +16,64 @@ function val(x) {
     return result;
 }
 
-var human = {staerke:1,geschick:1,magie:1,klugheit:1};
-var elf = {wille:2,magie:2};
-var dwarf = {staerke:1,geschick:1,konstitution:2};
-
-var warrior = {staerke:4,geschick:3,konstitution:3};
-var mage = {magie:5,wille:4,klugheit:1};
-var rogue = {geschick:4,wille:2,klugheit:4};
-
-var races = [human,elf,dwarf];
-var classes = [warrior,mage,rogue];
-
-for (var stat in stats) {
-    say(stat);
-    
-    say(elf[stat]);
-    say(stats[stat] + val(elf[stat]));
+var races = {
+    none: {},
+    human: {staerke:1,geschick:1,magie:1,klugheit:1},
+    elf: {wille:2,magie:2},
+    dwarf: {staerke:1,geschick:1,konstitution:2}
 }
 
-document.write("<table>");
+var professions = {
+    none: {},
+    warrior: {staerke:4,geschick:3,konstitution:3},
+    mage: {magie:5,wille:4,klugheit:1},
+    rogue: {geschick:4,wille:2,klugheit:4}
+}
 
-//document.write()
+document.write("<table border='1'>");
+
+document.write("<tr>");
+document.write("<td></td>");
+for (var race in races) {
+    document.write("<th>" + race + "</th>");
+}
+document.write("</tr>");
+
+for (var profession in professions) {
+    document.write("<tr>");
+    document.write("<th>" + profession + "</th>");
+    for (var race in races) {
+        document.write("<td>");
+        var total = 0;
+        document.write("<ul>");
+        for (var stat in stats) {
+            document.write("<li>");
+            document.write(stat + ": ");
+            //document.write(stats[stat]);
+            total += (val(professions[profession][stat]*stats[stat]));
+            total += (val(races[race][stat]*stats[stat]));
+            document.write(10 + val(professions[profession][stat]) + val(races[race][stat]));
+            document.write("</li>");
+           
+        }
+        document.write("</ul>");
+         document.write("total: " + total);
+        document.write("</td>");
+    }
+    document.write("<th>" + profession + "</th>");
+    document.write("</tr>");
+}
+
+document.write("<tr>");
+document.write("<td></td>");
+for (var race in races) {
+    document.write("<th>" + race + "</th>");
+}
+document.write("<td></td>");
+document.write("</tr>");
 
 document.write("</table>");
+
+for (var race in races) {
+    
+}
