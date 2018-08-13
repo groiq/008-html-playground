f1990fc0fca4f0910a8e246fe9af99dd8c09eaf9
groiq <35578027+groiq@users.noreply.github.com>
2018-08-13 11:27:08 +0200

    length of array with gaps

--- js/index.js
+++ js/index.js
@@ -1,17 +1,12 @@
-var x = new Array();
-x[0] = 1;
-x["name"] = "meier";
+var gaps = new Array();
 
+gaps[3] = "hello";
 
-say(x[0]);
-say(x["0"]);
-say(x[name]);
-say(x["name"]);
+list(gaps);
+
+say(gaps.length);
 
 /* output:
-1
-1
-undefined
-meier
-
+3: hello
+4
 */
