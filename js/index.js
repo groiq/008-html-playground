10e16d6af7ee9751e9c0f58cd78dd6a129156311
groiq <35578027+groiq@users.noreply.github.com>
2018-08-13 13:44:44 +0200

    replace string exercise

--- js/index.js
+++ js/index.js
@@ -1,61 +1,24 @@
-var keywords = ["gaps","gapsReversed","filled","filledReversed"];
-
-var arrays = new Array();
-
-for (var kwdIndex in keywords) {
-    var keyword = keywords[kwdIndex];
-    arrays[keyword] = new Array();
-    var arr = arrays[keyword];
-    arr[1] = "Hello";
-    arr[4] = "World";
-    if (keyword.match(/.*filled.*/i)) {
-        for (var i=0;i<arr.length;i++) {
-            if (!arr[i]) { arr[i] = 0; }
-        }
+function repOne(text,before,after) {
+    say(text + "; " + before + ": " + after);
+    var ind = text.indexOf(before);
+    while (ind > -1 ) {
+        var head = text.slice(0,ind);
+        var tail = text.slice((ind+1));
+        say(head);
+        say(tail);
+        ind = -1;
     }
-    if (keyword.match(/.*Reverse.*/i)) {
-        arr.reverse();
-    }
+    return text;
 }
 
-list(arrays);
-say("");
-
-for (var array in arrays) {
-    say(array + ": ");
-    list(arrays[array]);
+function replace(text) {
+    var replacements = {"ä":"ae","ö":"oe","ü":"ue"};
+    for (var replacement in replacements) {
+        text = repOne(text,replacement,replacements[replacement]);
+    }
+    return text;
 }
 
-/* output:
-gaps: ,Hello,,,World
-gapsReversed: World,,,Hello,
-filled: 0,Hello,0,0,World
-filledReversed: World,0,0,Hello,0
+var teststr = "1äöü12äöü23äöü3";
 
-gaps:
-1: Hello
-4: World
-gapsReversed:
-0: World
-3: Hello
-filled:
-0: 0
-1: Hello
-2: 0
-3: 0
-4: World
-filledReversed:
-0: World
-1: 0
-2: 0
-3: Hello
-4: 0
-6: !
-*/
-
-/*
-Things to do: Automate modification by flags:
-you have an object (called modifications or something) with entries such as "reverse:" x.reverse()
-for every entry in the modifications object you create one array where the modification is done and one where it isn't.
-That means that I'll have to write a "generate all possible test cases" script again!
-*/
+say(replace(teststr));
