6a00e5c714ec5f2cc71f364788e49a2b66edb94e
groiq <35578027+groiq@users.noreply.github.com>
2018-08-13 11:21:12 +0200

    index types in arrays

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
-var olds = ["human","elf","dwarf"];
+var x = new Array();
+x[0] = 1;
+x["name"] = "meier";
 
-var gw = ["asura","norn"];
 
-var got = ["white walker"];
+say(x[0]);
+say(x["0"]);
+say(x[name]);
+say(x["name"]);
 
-olds = olds.concat(gw,got);
+/* output:
+1
+1
+undefined
+meier
 
-for (var race in olds) { say(race + ": " + olds[race]);}
+*/
