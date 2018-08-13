90333bec4f4fdd28e79cbf3bd109c04ec6fc22e7
groiq <35578027+groiq@users.noreply.github.com>
2018-08-13 14:29:52 +0200

    substring replacement, using only string methods

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 function repOne(text,before,after) {
-    say(text + "; " + before + ": " + after);
+    //say(text + "; " + before + ": " + after);
     var ind = text.indexOf(before);
     while (ind > -1 ) {
         var head = text.slice(0,ind);
         var tail = text.slice((ind+1));
-        say(head);
-        say(tail);
-        ind = -1;
+        //say(head);
+        //say(tail);
+        text = head.concat(after);
+        text = text.concat(tail);
+        //say(text);
+        ind = text.indexOf(before);
+        //ind = -1;
     }
     return text;
 }
@@ -21,4 +25,5 @@ function replace(text) {
 
 var teststr = "1äöü12äöü23äöü3";
 
+say(teststr);
 say(replace(teststr));
