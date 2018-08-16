a8286831dde203fd4c71e721685408190c94bbec
groiq <35578027+groiq@users.noreply.github.com>
2018-08-16 13:33:29 +0200

    list() prints objects and writes them to console

--- js/say.js
+++ js/say.js
@@ -2,9 +2,15 @@ function say(text) {
     document.write(text + "<br />");
 };
 
+
 function list(array) {
+    // write simple objects to page
     for (var index in array) {
         say(index + ": " + array[index]);
     }
+    // pretty-print complex array to console
+    array = JSON.stringify(array,"---",2);
+    console.log(array);
+    
 }
 // things to do: write this function recursively
