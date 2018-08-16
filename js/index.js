41d88c2c1763e75d8559dc888a69548375f201fb
groiq <35578027+groiq@users.noreply.github.com>
2018-08-16 08:54:30 +0200

    loop through permutations: setup

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-var permutations = [
-    function(i) { return (i*2); },
-    function(i) { return (i*3); }
-]
+var possibleStates = [0,1];
+var permutations = {
+    byTwo: function(i) { return (i*2); },
+    byThree: function(i) {return (i*3); }
+};
 
-var i = 1;
-for (var permutation in permutations) {
-    i = permutations[permutation](i);
+
+
+for (var permutation in permutations ) {
+    say(permutation);
 }
-
-say(i);
