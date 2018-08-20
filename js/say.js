12feb7a2b61099a968a159b1ce7bf31c6b405b0b
groiq <35578027+groiq@users.noreply.github.com>
2018-08-20 12:26:39 +0200

    emptied list function

--- js/say.js
+++ js/say.js
@@ -6,7 +6,12 @@ function say(text) {
 function list(array) {
     // write simple objects to page
     for (var index in array) {
-        
+        //say(typeof array.index);
+        //say(typeof array[index]);
+        if (typeof array[index] == "object") {
+            //say("aha!");
+            list(array[index]);
+        }
         say(index + ": " + array[index]);
     }
     // pretty-print complex array to console
