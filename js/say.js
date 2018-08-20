8509445aab384d4dd2c783233b08837313002163
groiq <35578027+groiq@users.noreply.github.com>
2018-08-20 12:32:34 +0200

    solved the entire thing by using a bloody <pre> tag

--- js/say.js
+++ js/say.js
@@ -4,18 +4,12 @@ function say(text) {
 
 
 function list(array) {
-    // write simple objects to page
-    for (var index in array) {
-        //say(typeof array.index);
-        //say(typeof array[index]);
-        if (typeof array[index] == "object") {
-            //say("aha!");
-            list(array[index]);
-        }
-        say(index + ": " + array[index]);
-    }
+    
     // pretty-print complex array to console
     array = JSON.stringify(array,"---",2);
+    document.write("<pre>");
+    document.write(array);
+    document.write("</pre>");
     console.log(array);
     
 }
