f478b9ec716dc4af951c0663d14cf5b6e5b77ff5
groiq <35578027+groiq@users.noreply.github.com>
2018-08-21 09:17:23 +0200

    display text rahter than img on onclick

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
 function showBigImg(img) {
-    //alert(img);
-    /*var image = document.createElement("img");
-    var imgsrc = document.createAttribute("src");
-    imgsrc.nodeValue = img;
-    image.setAttributeNode(imgsrc);
-    var bod = document.getElementsByTagName("body")[0];
-    bod.appendChild(image);*/
-    var bigImg = document.getElementById("bigimg");
-    bigImg.src = img;
+    document.getElementById("imgInfo").innerHTML="hello";
 }
