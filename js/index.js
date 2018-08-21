1791cd2f12f319593d499a02672a6ac982bb4aae
groiq <35578027+groiq@users.noreply.github.com>
2018-08-21 10:15:10 +0200

    exercise for DOM - finished

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+var dispDuration = 5;
+
 function showBigImg(img) {
-    document.getElementById("imgInfo").innerHTML="hello";
+    var img = document.getElementById(img);
+    // alert(img.src);
+    infoText = function(i) { document.getElementById("imgInfo").innerHTML = i; };
+    // var infoText = document.getElementById("imgInfo").innerHTML;
+    // alert(infoText);
+    // infoText=img.src;
+    infoText(img.src);
+    // setTimeout(infoText('noInfo'), (5000));
+    // function myFunction() { alert('gehts noch??'); };
+    // setTimeout(myFunction, 3000);
+    function timesUp() {  infoText('&nbsp;'); };
+    setTimeout(timesUp, dispDuration*1000);
+    //var timesUp = function() {infoText='&nbsp;'};
+    //setTimeout(timesUp, (dispDuration * 1000));
+}
+
+function displayDuration() {
+    var newDuration = prompt("Anzeigedauer:");
+    if(newDuration > 0 && newDuration < 21) {
+        dispDuration = newDuration;
+    } else {
+        alert("bad input!");
+    }
+    document.getElementById("dispadur").innerHTML=dispDuration;
 }
