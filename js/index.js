0ac10cc8eae6971d30dee31977d96e0712b2126d
groiq <35578027+groiq@users.noreply.github.com>
2018-08-22 08:48:19 +0200

    parse form within javascript: done

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ window.onload=function() {
     
     document.getElementById("submit").onclick = parseForm;
     
-}
+};
 
 function parseForm() {
-    document.getElementById("result").innerHTML = document.getElementById("userText").innerHTML;
+    document.getElementById("result").innerHTML = document.getElementById("form1").userText.value;
 }
