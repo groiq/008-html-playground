9fd4ef25b87ab17bb9615159850442cc1a08ed4a
groiq <35578027+groiq@users.noreply.github.com>
2018-08-22 11:36:21 +0200

    check for obligatory fields

--- js/index.js
+++ js/index.js
@@ -9,8 +9,27 @@ window.onload=function() {
 function parseForm() {
     var result="";
     var form = document.getElementById("form1");
-    result += form.Name;
-    result += form.Gewicht;
+    function obligatory(field) {
+        if (field.value == "") {
+            result = result + "error: " + field.name + ": obligatory<br />";
+            field.focus();
+        } else {
+            result += field.value + "<br/>";
+        }
+    }
+    obligatory(form.Name);
+    obligatory(form.location);
+    /*if (form.Name.value == "") {
+        result += "error: please write a name";
+        form.Name.focus();
+    }*/
+    function checkForm(field) {
+        var reference = "1234567890";
+        for (var c in field.value) {
+            
+        }
+    }
+    result += form.Gewicht.value;
    
 
     document.getElementById("result").innerHTML = result;
