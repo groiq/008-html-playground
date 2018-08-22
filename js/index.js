dc15a69fb96595fa7405af757dc3f804a5594a28
groiq <35578027+groiq@users.noreply.github.com>
2018-08-22 09:15:29 +0200

    set up form selection

--- js/index.js
+++ js/index.js
@@ -3,12 +3,21 @@ window.onload=function() {
     var weapons = ["sword","shield","bow","staff","axe"];
     var usages = ["melee","defensive","ranged","supportive","conditions"];
     
-    
+   for (var i in weapons) {
+       var tag = document.createElement("option");
+       var tagValue = document.createAttribute("value");
+       tagValue.nodeValue = i;
+       tag.innerHTML = weapons[i];
+       
+       document.getElementById("weapons").appendChild(tag);
+   }     
+        
     
     document.getElementById("submit").onclick = parseForm;
     
 };
 
 function parseForm() {
-    document.getElementById("result").innerHTML = document.getElementById("form1").userText.value;
+    
+   
 }
