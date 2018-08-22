7bc815c73727733d7f664d9eceec8af854eb85bf
groiq <35578027+groiq@users.noreply.github.com>
2018-08-22 09:31:21 +0200

    reset code

--- js/index.js
+++ js/index.js
@@ -1,16 +1,5 @@
 window.onload=function() {
     
-    var weapons = ["sword","shield","bow","staff","axe"];
-    var usages = ["melee","defensive","ranged","supportive","conditions"];
-    
-   for (var i in weapons) {
-       var tag = document.createElement("option");
-       var tagValue = document.createAttribute("value");
-       tagValue.nodeValue = i;
-       tag.innerHTML = weapons[i];
-       
-       document.getElementById("weapons").appendChild(tag);
-   }     
         
     
     document.getElementById("submit").onclick = parseForm;
@@ -18,23 +7,8 @@ window.onload=function() {
 };
 
 function parseForm() {
-    // document.getElementById("result").innerHTML = document.getElementById("form1").userText.value;
-    var result = "You have covered these use cases: ";
-    with (document.getElementById("form1").weapons) {
-        for (var option in options) {
-            result += option;
-            result += options[option];
-        }
-    }
-   /* for (var option in document.getElementById("form1").weapons.options) {
-        //result += option;
-        if (option.selected) {
-            result += option;
-        }
-    }*/
-    
-    
+    var result="";
+
     document.getElementById("result").innerHTML = result;
-    //document.getElementById("form1").weapons.value;
-   
+    
 }
