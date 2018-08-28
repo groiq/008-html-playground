d4261fb91d3cd1afda08b76ebe10f6694a2033d1
groiq <35578027+groiq@users.noreply.github.com>
2018-08-28 10:02:53 +0200

    testing console log

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 window.onload=function() {
     
-    console.log("Hallo Welt!");    
-    
+   a = 2;
+console.log(a);
+b = 3;
+console.log(b);
+c = a*a + b*b;
+console.log(c);
+window.alert(ergebnis);
     
 };
