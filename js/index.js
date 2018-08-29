c3630ef0e6b01b3c9cadc1097185dd0ee1135a49
groiq <35578027+groiq@users.noreply.github.com>
2018-08-29 10:45:17 +0200

    test error message: done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
 window.onload = function() {
     var output = "set";
     
+    // testLocalVars(output);
+    
     window.addEventListener("error", fehlerbehandlung);
+    // window.addEventListener("load", testLocalVars());
+    // testLocalVars();
+    nichtDa();
     
     
-    document.getElementById("output").innerHTML = output;
+    // document.getElementById("output").innerHTML = output;
 };
+
+function fehlerbehandlung(errorEvent) {
+    output = "fehler!";
+    output += errorEvent.message + "<br />" + errorEvent.filename;
+    document.getElementById("output").innerHTML = output;
+    
+}
+
+function testLocalVars() {
+    alert("here!");
+    output = "output set locally";
+    document.getElementById("output").innerHTML = output;
+}
