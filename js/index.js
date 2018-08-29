27facb972e7e2699232a2b03e6b3d1729c358963
groiq <35578027+groiq@users.noreply.github.com>
2018-08-29 12:47:29 +0200

    code scaffolding

--- js/index.js
+++ js/index.js
@@ -10,23 +10,8 @@ function doSomething() {
     
     var result = "";
     
-    // result = "set in function";
+    result = "set in function";
     
-    function append(result,x) {
-        var y;
-        try {
-            y = x;
-        } catch {
-            y = "undefined";
-        } finally {
-            result += "x is " + y + ".<br />";
-        }
-        return result;
-    }
-    
-    result = append(result,x);
-    var x = 5;
-    result = append(result,x);
     
     return result;
 }
