667ec4153a6790c2889007efd5c3500ee5f93c59
groiq <35578027+groiq@users.noreply.github.com>
2018-08-29 12:28:48 +0200

    test try-catch: setup

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
-window.onload = function() {
-    var output = "set";
-    
-    // testLocalVars(output);
-    
-    window.addEventListener("error", fehlerbehandlung);
-    // window.addEventListener("load", testLocalVars());
-    // testLocalVars();
-    nichtDa();
-    
-    
-    // document.getElementById("output").innerHTML = output;
-};
-
-function fehlerbehandlung(errorEvent) {
-    output = "fehler!";
-    output += errorEvent.message + "<br />" + errorEvent.filename;
-    document.getElementById("output").innerHTML = output;
+function assign(x) {
+    alert("hello");
     
 }
 
-function testLocalVars() {
-    alert("here!");
-    output = "output set locally";
-    document.getElementById("output").innerHTML = output;
-}
+assing(x);
+var x = 5;
+assign(x);
